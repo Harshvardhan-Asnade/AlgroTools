baba14771f419c9050f2abd9a5097bea1baf17bb
Harsh Asnade <harshasnade24@gmail.com>
2025-08-18 11:00:55 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="contai

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ import { Logo } from '@/components/logo';
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/80 backdrop-blur-sm">
+    <header className="sticky top-0 z-50 w-full bg-background/80 backdrop-blur-sm">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
